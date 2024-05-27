675cdfe966f59904bef2263a0e42d6e14564c925
Jhosep Arellano <josepharellanocastillo@gmail.com>
2024-05-27 08:22:52 -0500

    Creacion de controlador de imagenes para cargar en vistas del front

---
+++ src/main/java/com/app/Backend/controller/ImageController.java
@@ -0,0 +1,57 @@
+package com.app.Backend.controller;
+
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.MalformedURLException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+@RestController
+@RequestMapping("/api/images")
+public class ImageController {
+
+    private final Path imagePathProduct = Paths.get("images/Productos/");
+    private final Path imagePathStore = Paths.get("images/Tiendas/");
+
+    @GetMapping("/producto/{filename:.+}")
+    @ResponseBody
+    public ResponseEntity<Resource> getImageProduct(@PathVariable String filename) {
+        try {
+            Path file = imagePathProduct.resolve(filename);
+            Resource resource = new UrlResource(file.toUri());
+
+            if (resource.exists() || resource.isReadable()) {
+                return ResponseEntity.ok()
+                        .contentType(MediaType.IMAGE_JPEG)
+                        .body(resource);
+            } else {
+                throw new RuntimeException("Could not read the file!");
+            }
+        } catch (MalformedURLException e) {
+            throw new RuntimeException("Error: " + e.getMessage());
+        }
+    }
+
+    @GetMapping("/tienda/{filename:.+}")
+    @ResponseBody
+    public ResponseEntity<Resource> getImageStore(@PathVariable String filename) {
+        try {
+            Path file = imagePathStore.resolve(filename);
+            Resource resource = new UrlResource(file.toUri());
+
+            if (resource.exists() || resource.isReadable()) {
+                return ResponseEntity.ok()
+                        .contentType(MediaType.IMAGE_JPEG)
+                        .body(resource);
+            } else {
+                throw new RuntimeException("Could not read the file!");
+            }
+        } catch (MalformedURLException e) {
+            throw new RuntimeException("Error: " + e.getMessage());
+        }
+    }
+}
